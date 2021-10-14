0c80b74fd9b6373d2297cb445abaed91d1bc2ae8
Blake Stickland <blakestickland@gmail.com>
2021-10-14 22:35:17 +1100

    tests and logic added to check the user input and automatically sense if input is ealphanumeric or morse code and translate accordingly

--- scripts/checkIfEnglish.js
+++ scripts/checkIfEnglish.js
@@ -1,17 +1,22 @@
 
 export const checkIfEnglish = (text) => {
-  let isEnglishText = false;
-  const regex = /[A-Za-z0-9]/;
+    // should throw error if no arguments
+    if (text === undefined) {
+        throw new Error("No args found");
+    }
 
-  // should throw error if no arguments
-  if (text.trim() === undefined) {
-    throw new Error("No args found");
-  }
+    // should check all characters in the userInput for alphanumeric characters
+    // and symbols other than morse code symbols.
+    let isEnglishText = true;
+    const regex = /[A-Za-z0-9,?'()&:;=+_"$!@]/;
 
-  // should check all characters in the userInput for alphanumeric characters
-  // and symbols other than morse code symbols.
-//   text.split("").map((char) => {
-//     char.match(regex) ? isEnglishText : isEnglishText === false;
-//   });
-  return isEnglishText;
+    text.replace(/\s/g)
+        .split("")
+        .map((char) => {
+            char.match(regex) ? 
+            isEnglishText : 
+            isEnglishText = false;
+        });
+
+    return isEnglishText;
 };
